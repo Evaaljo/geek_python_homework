d3d132f886fa2cddb406b88ea278db05ffa39a37
Evgeniya Aleshkina <92468635+Evaaljo@users.noreply.github.com>
2022-01-15 22:36:56 +0300

    Add files via upload

---
+++ exercise 01.py
@@ -0,0 +1,12 @@
+# Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+integer_variable = 7
+float_variable = 3.5
+string_variable = "Зимняя пора"
+bool_variable = False
+
+user_city = input("Укажите ваш город: ")
+print(user_city)
+user_age = int(input("Укажите ваш возраст: "))
+print(user_age)
