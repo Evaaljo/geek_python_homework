7d1f2bea0cac6a6ba121803f8d2b702de3fe1912
Evgeniya Aleshkina <92468635+Evaaljo@users.noreply.github.com>
2022-03-20 04:15:00 +0300

    Create exercise 01.py

---
+++ Homework_08/exercise 01.py
@@ -0,0 +1,31 @@
+"""Реализовать класс «Дата», функция-конструктор которого должна принимать дату в виде строки формата
+«день-месяц-год». В рамках класса реализовать два метода. Первый, с декоратором @classmethod, должен извлекать число,
+месяц, год и преобразовывать их тип к типу «Число». Второй, с декоратором @staticmethod, должен проводить валидацию
+числа, месяца и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных. """
+
+
+class Date:
+    day: int
+    month: int
+    year: int
+
+    def __init__(self, date_string: str):
+        numbers = Date.extract_numbers(date_string)
+        self.validate_numbers(numbers)
+
+        self.day, self.month, self.year = numbers
+
+    @classmethod
+    def extract_numbers(cls, date_string: str) -> list:
+        return [int(x) for x in date_string.split('-')]
+
+    @staticmethod
+    def validate_numbers(numbers: list):
+        d, m, y = numbers
+
+        assert 1 <= d <= 31, "Неверно введен день"
+        assert 1 <= m <= 12, "Неверно введен месяц"
+        assert 1900 <= y <= 2022, "Неверно введен год"
+
+my_date = Date('17-05-1991')
+print(my_date)
